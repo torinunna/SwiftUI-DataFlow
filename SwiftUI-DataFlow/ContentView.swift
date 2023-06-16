d5072970c6fee6e4f79b16df4899d9222817609f
torinunna <torinunna9588@gmail.com>
2023-06-16 12:31:17 +0900

    Initial Commit

---
+++ SwiftUI-DataFlow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-DataFlow
+//
+//  Created by YUJIN KWON on 2023/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-DataFlow/SwiftUI_DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DataFlowApp.swift
+//  SwiftUI-DataFlow
+//
+//  Created by YUJIN KWON on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
